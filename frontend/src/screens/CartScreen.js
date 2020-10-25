4633a04f70c99967b522e61d930665e585840a1f
Alexey2001 <andr200116alex@gmail.com>
2020-10-25 21:42:24 +0300

    remove users

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Form,
 } from "react-bootstrap";
 import Message from "../components/Message";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -28,6 +28,10 @@ const CartScreen = ({ match, location, history }) => {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
+  }
 
   const checkoutHandler = () => {
     history.push("/login?redirect=shipping");
@@ -74,7 +78,7 @@ const CartScreen = ({ match, location, history }) => {
                     <Button
                       type="button"
                       variant="light"
-                      onClick={() => console.log("lol")}
+                      onClick={() => removeFromCartHandler(item.product)}
                     >
                       <i className="fas fa-trash"></i>
                     </Button>
