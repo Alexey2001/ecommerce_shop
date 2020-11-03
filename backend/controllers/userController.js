910c84823f0943fa17a4312cd2347dc94841167f
alexey <andr200116alex@gmail.com>
2020-11-03 12:57:54 +0300

    user register reducer, action & screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,14 @@
-import asyncHandler from "express-async-handler";
-import generateToken from "../utils/generateToken.js";
-import User from "../models/userModel.js";
+import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -17,22 +17,22 @@ const authUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
 // @desc    Register a new user
-// @route   POST /api/users/login
+// @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body
 
-  const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email })
 
-  if(userExists) {
+  if (userExists) {
     res.status(400)
     throw new Error('User already exists')
   }
@@ -40,10 +40,10 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password
+    password,
   })
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -55,13 +55,13 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Invalid user data')
   }
-});
+})
 
 // @desc    Get user profile
-// @route   POST /api/users/profile
+// @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id)
 
   if (user) {
     res.json({
@@ -69,11 +69,11 @@ const getUserProfile = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("User not found");
+    res.status(404)
+    throw new Error('User not found')
   }
-});
+})
 
-export { authUser, registerUser,getUserProfile };
+export { authUser, registerUser, getUserProfile }
