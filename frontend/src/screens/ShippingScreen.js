91acf6541b96efc99b31f84ade78491aa6e7ea8c
alexey <andr200116alex@gmail.com>
2020-11-04 14:22:10 +0300

    register screen 1/2

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+
+const ShippingScreen = ({ history }) => {
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+
+  return <FormContainer>
+  <h1>Shipping</h1>
+  </FormContainer>;
+};
+
+export default ShippingScreen;
