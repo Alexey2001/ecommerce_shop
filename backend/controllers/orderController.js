5c863252a3ed18f883750a33f5f295c3a42f1423
alexey <andr200116alex@gmail.com>
2020-11-09 22:33:18 +0300

    create order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,41 +1,41 @@
-import asyncHandler from "express-async-handler";
-import Order from "../models/orderModel.js";
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
 
 // @desc    Create new order
-// @route   GET /api/orders
+// @route   POST /api/orders
 // @access  Private
-
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
-    orederItems,
+    orderItems,
     shippingAddress,
     paymentMethod,
-    itemPrice,
+    itemsPrice,
     taxPrice,
     shippingPrice,
     totalPrice,
-  } = req.body;
+  } = req.body
 
-  if(orderItems && orderItems.length === 0) {
-      res.status(400)
-      throw new Error('No order items')
-      return
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No order items')
+    return
   } else {
-      const order = new Order({
-        orederItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      })
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
 
-      const createdOrder = await order.save()
+    const createdOrder = await order.save()
 
-      res.status(201).json(createdOrder)
+    res.status(201).json(createdOrder)
   }
-});
+})
 
 export { addOrderItems }
