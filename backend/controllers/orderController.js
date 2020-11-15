6fdacd64c698f642ae7ac5fbec3d8aa90f1f5f23
alexey <andr200116alex@gmail.com>
2020-11-15 23:48:47 +0300

    show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,7 +67,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email_address
+      email_address: req.body.payer.email_address,
     }
 
     const updatedOrder = await order.save()
@@ -83,7 +83,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({user: req.user._id})
+  const orders = await Order.find({ user: req.user._id })
   res.json(orders)
 })
 
